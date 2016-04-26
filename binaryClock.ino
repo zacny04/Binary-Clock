d9e1a8feeaf11d8fe4a2ce8530b6e3c391ed073c
zacny04 <zacny04@users.noreply.github.com>
2016-04-26 22:51:35 +0200

    fix seconds

--- binaryClock.ino
+++ binaryClock.ino
@@ -91,6 +91,7 @@ void loop() {
       if(mButtonState==HIGH) {
         now.setMinute(now.Minute() + 1);
         now.setSecond(0);
+        s = now.Second();
       Rtc.SetDateTime(now);
       Serial.println("Minute++ "+(String)m);
       }
