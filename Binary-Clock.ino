03808e472152f5050890d9b5907de7758edb9d6d
zacny04 <zacny04@users.noreply.github.com>
2016-05-10 12:48:22 +0200

    Use one array insetead of two
    We don't have to use 2 arrays for the same thing.

--- Binary-Clock.ino
+++ Binary-Clock.ino
@@ -15,15 +15,14 @@ setHour() and setMinute() require changes in Rtc library, they have to be added 
 
 RtcDS1307 Rtc;
 
-byte Array[6][4][2] {
-  {{A, B}, {B, A}},
-  {{C, A}, {A, C}, {B, C}, {C, B}},
-  {{D, A}, {A, D}, {D, C}},
-  {{A, E}, {E, D}, {D, E}, {C, D}},
-  {{E, A}, {B, D}, {D, B}},
-  {{C, E}, {E, C}, {B, E}, {E, B}}
+byte ledArray[6][4][3] {
+  {{A, B, 0}, {B, A, 0}},
+  {{C, A, 0}, {A, C, 0}, {B, C, 0}, {C, B, 0}},
+  {{D, A, 0}, {A, D, 0}, {D, C, 0}},
+  {{A, E, 0}, {E, D, 0}, {D, E, 0}, {C, D, 0}},
+  {{E, A, 0}, {B, D, 0}, {D, B, 0}},
+  {{C, E, 0}, {E, C, 0}, {B, E, 0}, {E, B, 0}}
 };
-bool arrayBinary[6][4];
 byte s = 0, m = 0, h = 0;
 int hLastState = 0, mLastState=0;
 long unsigned time = millis();
@@ -32,10 +31,10 @@ void toBin(int a, int j) {
   int i = 0;
   if (a == 0) {
     for (i; i < 4; i++)
-      arrayBinary[j][i] = 0;
+      ledArray[j][i][3] = 0;
   } else {
     while (a > 0) {
-      arrayBinary[j][i++] = a % 2;
+      ledArray[j][i++][3] = a % 2;
       a /= 2;
     }
   }
@@ -65,10 +64,10 @@ void setup() {
 void loop() {
   RtcDateTime now = Rtc.GetDateTime();
 
-  int pom[6] = { h/10, h%10, m/10,
+  int numbers[6] = { h/10, h%10, m/10,
                  m%10, s/10, s%10 };
   for (int i = 0; i < 6; i++) {
-    toBin(pom[i], i);
+    toBin(numbers[i], i);
   }
 
   while (time + 1000 > millis()) {
@@ -103,15 +102,15 @@ void loop() {
     
     for (int i = 0; i < 6; i++)
       for (int j = 0; j < 4; j++) {
-        if (arrayBinary[i][j]) {
-          pinMode(Array[i][j][0], OUTPUT);
-          pinMode(Array[i][j][1], OUTPUT);
-          digitalWrite(Array[i][j][0], HIGH);
+        if (ledArray[i][j][3]==1) {
+          pinMode(ledArray[i][j][0], OUTPUT);
+          pinMode(ledArray[i][j][1], OUTPUT);
+          digitalWrite(ledArray[i][j][0], HIGH);
           //led workaround
           if (i % 2 == 0)
             delay(0.9);
-          pinMode(Array[i][j][0], INPUT);
-          pinMode(Array[i][j][1], INPUT);
+          pinMode(ledArray[i][j][0], INPUT);
+          pinMode(ledArray[i][j][1], INPUT);
         }
       }
     
